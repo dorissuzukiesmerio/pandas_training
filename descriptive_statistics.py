8b9ecf7126b8aa7b334735e1d0811da49ac94441
dorissuzukiesmerio <dsuzuki@g.clemson.edu>
2021-07-27 18:34:39 -0400

    corrected syntax

--- descriptive_statistics.py
+++ descriptive_statistics.py
@@ -25,62 +25,62 @@ df2 = pd.DataFrame(
 # >>>Function Description
 
 print("\nNumber of non-NA observations")
-print(df.count("a"> "8"))
+print(df.count("a"> "8")())
 
 print("\nSum of values")
-print(df.sum)
+print(df.sum())
 
 print("\nMean of values")
-print(df.mean)
+print(df.mean())
 
 print("\nMean absolute deviation")
-print(df.mad)
+print(df.mad())
 
 print("\nArithmetic median of values")
-print(df.median)
+print(df.median())
 
 print("\nMinimum")
-print(df.min)
+print(df.min())
 
 print("\nMaximum")
-print(df.max)
+print(df.max())
 
 print("\nMode")
-print(df.mode)
+print(df.mode())
 
 print("\nAbsolute Value")
-print(df.abs)
+print(df.abs())
 
 print("\nProduct of values")
-print(df.prod)
+print(df.prod())
 
 print("\nBessel-corrected sample standard deviation")
-print(df.std)
+print(df.std())
 
 print("\nUnbiased variance")
-print(df.var)
+print(df.var())
 
 print("\nStandard error of the mean")
-print(df.sem)
+print(df.sem())
 
 print("\nSample skewness (3rd moment)")
-print(df.skew)
+print(df.skew())
 
 print("\nSample kurtosis (4th moment")
-print(df.kurt)
+print(df.kurt())
 
 
 print("\nSample quantile (value at %)")
-print(df.quantile)
+print(df.quantile())
 
 print("\nCumulative sum")
-print(df.cumsum)
+print(df.cumsum())
 
 print("\nCumulative product")
-print(df.cumprod)
+print(df.cumprod())
 
 print("\nCumulative maximun")
-print(df.cummax)
+print(df.cummax())
 
 print("\nCumulative minimun")
-print(df.cummin)
+print(df.cummin())
